b2b4df6db99c7120e7260eeed5b0957d75cd9272
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2023-02-21 10:45:02 +0200

    auth context

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZ-WiG7CHrdmJJS3h2bSgiIAqt0Uf36gc",
+  authDomain: "mymoney-39b88.firebaseapp.com",
+  projectId: "mymoney-39b88",
+  storageBucket: "mymoney-39b88.appspot.com",
+  messagingSenderId: "76478018079",
+  appId: "1:76478018079:web:1d71c39bd6a8ff23be1b2a",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+export { projectFirestore, projectAuth };
